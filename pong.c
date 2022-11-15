da29b648572c441095c63e69c1bf86a58fb17f9d
Neutron-Cntrl <hugo.lindfors2@elev.ga.ntig.se>
2022-11-15 09:39:28 +0100

    further changes

--- pong.c
+++ pong.c
@@ -1,5 +1,7 @@
 #include "raylib.h"
 
+// cc pong.c `pkg-config --libs --cflags raylib` -o pong
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -13,6 +15,13 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
 
     Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+    Vector2 ballVelocity = { 2.0f, 1.0f };
+
+    Vector2 racket1Position = { 80, (float)screenHeight/2 };
+    Vector2 racket1Velocity = { 0.0f, 2.0f };
+
+    Vector2 racket2Position = { 720, (float)screenHeight/2 };
+    Vector2 racket2Velocity = { 0.0f, 2.0f };
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -22,21 +31,45 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+        ballPosition.x += ballVelocity.x;
+        ballPosition.y += ballVelocity.y;
+
+        if (IsKeyDown(KEY_S)) racket1Position.y += racket1Velocity.y;
+        if (IsKeyDown(KEY_DOWN)) racket2Position.y += racket2Velocity.y;
+
+        if (IsKeyDown(KEY_W)) racket1Position.y -= racket1Velocity.y;
+        if (IsKeyDown(KEY_UP)) racket2Position.y -= racket2Velocity.y;
+
+        if (ballPosition.x >= racket1Position.x - 5) 
+        {
+            if (ballPosition.y >= racket1Position.y)
+            {
+                ballVelocity.x = -(ballVelocity.x);
+                ballVelocity.y = -(ballVelocity.y);   
+            }
+        }
+
+        if (ballPosition.x <= racket2Position.x + 5)
+        {
+            if (ballPosition.y <= racket2Position.y)
+            {
+                ballVelocity.x = -(ballVelocity.x);
+                ballVelocity.y = -(ballVelocity.y);
+            }
+        }
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(BLACK);
 
-            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+            DrawRectangle(racket1Position.x - 5, racket1Position.y - 100, 10, 200, RED);
 
-            DrawCircleV(ballPosition, 50, MAROON);
+            DrawRectangle(racket2Position.x + 5, racket2Position.y - 100, 10, 200, WHITE);
+
+            DrawRectangle(ballPosition.x - 10, ballPosition.y - 10, 20, 20, WHITE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
